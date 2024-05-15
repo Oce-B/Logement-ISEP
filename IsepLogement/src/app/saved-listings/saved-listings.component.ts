2c630de69b4ce66fe70e26d6ed6bf5bc76b6f487
Oce-B <oceane.brigitte-raffin@eleve.isep.fr>
2024-05-15 11:45:50 +0200

    adding saved listings component

---
+++ IsepLogement/src/app/saved-listings/saved-listings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-listings',
+  standalone: true,
+  imports: [],
+  templateUrl: './saved-listings.component.html',
+  styleUrl: './saved-listings.component.scss'
+})
+export class SavedListingsComponent {
+
+}
